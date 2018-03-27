40d3d88613e5026e094be1739ecb70a8ff7454cc
Michael Nieß <randusr@gmx.de>
2018-03-27 11:41:42 +0200

    fix run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-cd "${0%/*}/BuildingSim"
+pwd
+cd "${0%/*}/../BuildingSim"
+pwd
 java -Djava.library.path=build/embedding/nsga2 -jar ../PopCodeGA/target/PopCodeGA-1.0-SNAPSHOT-jar-with-dependencies.jar "$(hostname)"
 
 
